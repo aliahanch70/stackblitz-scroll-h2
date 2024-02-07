543844911f891c5eb6d7a348bf9cfa11b1b859e7
aliahanch70 <86826859+aliahanch70@users.noreply.github.com>
2024-02-07 08:04:26 +0000

    product details on going!

--- my-app/app/(front)/page.tsx
+++ my-app/app/(front)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <h2 className="text-2xl py-2">Latest Product</h2>
-      <div className="grid grid-cols-2 gap-3 md:grid-cols-4 lg:grid-cols-5">
+      <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
         {data.products.map((product)=>(
           <ProductItem key={product.slug} product={product}/>
         ))}
